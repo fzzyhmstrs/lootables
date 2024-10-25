8d155ccd12a9c938ba1737b94e759efac2726769
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-10-25 12:04:05 -0400

    Update ChoicesScreen.kt

--- src/main/kotlin/me/fzzyhmstrs/lootables/client/screen/ChoicesScreen.kt
+++ src/main/kotlin/me/fzzyhmstrs/lootables/client/screen/ChoicesScreen.kt
@@ -17,7 +17,7 @@ import me.fzzyhmstrs.lootables.client.LootablesClientData
 import me.fzzyhmstrs.lootables.network.ChoicesS2CCustomPayload
 import net.minecraft.client.gui.screen.Screen
 
-class ChoicesScreen(private val choiceData: ChoicesS2CCustomPayload, private var choicesLeft: Int, private val oldScreen: Screen?): Screen(FcText.empty()) {
+class ChoicesScreen(private val choiceData: ChoicesS2CCustomPayload, private var choicesLeft: Int, internal val oldScreen: Screen?): Screen(FcText.empty()) {
 
     override fun close() {
         this.client?.setScreen(oldScreen)
