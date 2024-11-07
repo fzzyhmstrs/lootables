23e0b163385205e1352a9a1fb3856c105eb496a9
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-11-07 11:39:40 -0500

    Create StatusEffectLootablePoolEntry.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/lootables/loot/entry/StatusEffectLootablePoolEntry.kt
@@ -0,0 +1,78 @@
+/*
+ *
+ *  Copyright (c) 2024 Fzzyhmstrs
+ *
+ *  This file is part of Lootables API , a mod made for minecraft; as such it falls under the license of Lootables API.
+ *
+ *  Lootables API is free software provided under the terms of the Timefall Development License - Modified (TDL-M).
+ *  You should have received a copy of the TDL-M with this software.
+ *  If you did not, see <https://github.com/fzzyhmstrs/Timefall-Development-Licence-Modified>.
+ *
+ */
+
+package me.fzzyhmstrs.lootables.loot.entry
+
+import com.mojang.serialization.Codec
+import com.mojang.serialization.MapCodec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+import me.fzzyhmstrs.fzzy_config.util.FcText.translate
+import me.fzzyhmstrs.lootables.loot.LootablePoolEntry
+import me.fzzyhmstrs.lootables.loot.LootablePoolEntryDisplay
+import me.fzzyhmstrs.lootables.loot.LootablePoolEntryType
+import me.fzzyhmstrs.lootables.loot.LootablePoolEntryTypes
+import me.fzzyhmstrs.lootables.loot.display.ItemLootablePoolEntryDisplay
+import me.fzzyhmstrs.lootables.loot.display.PoolLootablePoolEntryDisplay
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.loot.LootPool
+import net.minecraft.loot.LootTable
+import net.minecraft.loot.LootTables
+import net.minecraft.loot.context.LootContext
+import net.minecraft.loot.context.LootContextParameterSet
+import net.minecraft.loot.context.LootContextParameters
+import net.minecraft.loot.context.LootContextTypes
+import net.minecraft.registry.entry.RegistryEntry
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.Text
+import net.minecraft.util.ItemScatterer
+import net.minecraft.util.math.Vec3d
+import java.util.*
+import java.util.function.Consumer
+
+class StatusEffectLootablePoolEntry(private val instance: StatusEffectInstance): LootablePoolEntry {
+
+    override fun type(): LootablePoolEntryType {
+        return LootablePoolEntryTypes.STATUS
+    }
+
+    override fun apply(player: PlayerEntity, origin: Vec3d) {
+        player.addStatusEffect(StatusEffectInstance(instance))
+    }
+
+    override fun defaultDescription(): Text {
+        return "lootables.entry.status".translate(getStatusEffectDescription(instance), instance.duration)
+    }
+
+    private fun getStatusEffectDescription(statusEffect: StatusEffectInstance): Text {
+        val mutableText = statusEffect.effectType.value().name.copy()
+        if (statusEffect.amplifier in 1..9) {
+            mutableText.append(ScreenTexts.SPACE)
+                .append(Text.translatable("enchantment.level." + (statusEffect.amplifier + 1)))
+        }
+        return mutableText
+    }
+
+    override fun createDisplay(playerEntity: ServerPlayerEntity): LootablePoolEntryDisplay {
+        return HealLootablePoolEntryDisplay
+    }
+
+    companion object {
+
+        val CODEC: MapCodec<StatusEffectLootablePoolEntry> = RecordCodecBuilder.mapCodec { instance: RecordCodecBuilder.Instance<StatusEffectLootablePoolEntry> ->
+            instance.group(
+                StatusEffectInstance.CODEC.fieldOf("status").forGetter(StatusEffectLootablePoolEntry::instance)
+            ).apply(instance, ::StatusEffectLootablePoolEntry)
+        }
+    }
+
+}
